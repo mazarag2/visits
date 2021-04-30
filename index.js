05256712eb18406ef978f1e197c414b160d4ebea
mazarag2 <mazarag2@gmail.com>
2021-04-30 16:21:48 -0700

    correct cache key

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const redis = require('redis')
 
 const app = express();
 const client = redis.createClient({
-    host : 'redis-server',
-    port : 6379
+    host: 'redis-server',
+    port: 6379
 });
 
+client.set('visits',0);
 
 app.get('/',(req,res) => {
-    client.get('vists',(err,visits) => {
+    client.get('visits',(err,visits) => {
         res.send('Number of visits' + visits)
         client.set('visits',parseInt(visits) + 1)
     })
